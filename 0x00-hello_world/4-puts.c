b534a28e9c572397a644bb930f0ad6e1886333d6
danisw23 <daswitch6838@gmail.com>
2022-07-01 02:14:47 -0700

    hello c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+*use a function puts 
+*You are not allowed to use printf
+*Your program should end with the value 0
+*/
 int main(void)
-
 {
 puts("Programming is like building a multilingual puzzle\n");
 return (0);
